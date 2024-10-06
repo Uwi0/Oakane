a4e5a8e7b28b5aaee7dbda5eb43cbbfd93ae3032
uwi0 <kakapoweb@gmail.com>
2024-10-06 16:06:29 +0700

    add adjustment in progress view

--- iosApp/iosApp/designSystem/component/ProgressIndicatorView.swift
+++ iosApp/iosApp/designSystem/component/ProgressIndicatorView.swift
@@ -4,14 +4,14 @@ struct ProgressIndicatorView: View {
     @State private var animatedProgress: Float = 0.0
     var value: Float
     
-    private let thickness: CGFloat = 12
+    private let radius: CGFloat = 12
     
     var body: some View {
         ProgressView(value: animatedProgress, total: 1.0)
             .progressViewStyle(LinearProgressViewStyle())
+            .controlSize(.large)
             .tint(Oakane.primary)
-            .frame(width: .infinity,height: thickness)
-            .cornerRadius(thickness)
+            .cornerRadius(radius)
             .onAppear {
                 withAnimation(.easeInOut(duration: 0.5)) {
                     animatedProgress = value
