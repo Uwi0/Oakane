def1df725fe31991eaeb02c15b94254b410e9eaa
uwi0 <kakapoweb@gmail.com>
2024-11-08 17:33:09 +0700

    displaying custom image category in CategoryItemView

--- composeApp/src/androidMain/kotlin/com/kakapo/oakane/presentation/feature/categories/component/sheet/CreateCategoryContentView.kt
+++ composeApp/src/androidMain/kotlin/com/kakapo/oakane/presentation/feature/categories/component/sheet/CreateCategoryContentView.kt
@@ -1,7 +1,6 @@
 package com.kakapo.oakane.presentation.feature.categories.component.sheet
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
--- iosApp/iosApp/screen/categories/component/CategoryItemView.swift
+++ iosApp/iosApp/screen/categories/component/CategoryItemView.swift
@@ -9,7 +9,15 @@ struct CategoryItemView: View {
     var body: some View {
         HStack(spacing: 16) {
             let iconCategory = category.iconName.asIconCategory()
-            CategoryIconView(icon: iconCategory, color: Color(hex: category.formattedColor))
+            if category.isDefault {
+                CategoryIconView(icon: iconCategory, color: Color(hex: category.formattedColor))
+            } else {
+                DisplayImageFileView(fileName: category.icon, width: 48, height: 48)
+                    .overlay{
+                        Circle()
+                            .stroke(Color(hex: category.formattedColor), lineWidth: 3)
+                    }
+            }
             Text(category.name)
             Spacer()
         }
