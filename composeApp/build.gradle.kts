e165c0134f2864f5467acbdb19539703410e6d30
uwi0 <kakapoweb@gmail.com>
2025-01-20 11:52:00 +0700

    increase version for update play store

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -58,8 +58,8 @@ android {
         applicationId = "com.kakapo.oakane"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
     packaging {
         resources {
