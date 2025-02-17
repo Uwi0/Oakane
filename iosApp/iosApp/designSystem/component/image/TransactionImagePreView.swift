0212d3443b4bb2cc3b30b47ee83a9261af553ca9
uwi0 <kakapoweb@gmail.com>
2025-02-17 16:44:38 +0700

    feat: implement transaction image preview
    This commit introduces a new component, `TransactionImagePreView`, for displaying and managing images within transactions.
    
    -   **Image Loading:**
        -   The component loads images from the device's photo album using a provided `imageUrl`.
        -   The image is loaded asynchronously using the `loadImageFromAlbum` function and displayed using `Image`.
    -   **Fallback Image:**
        -   If the image fails to load, it defaults to displaying the `fubukiStare` image.
    -   **Dismiss Button:**
        -   Includes an optional dismiss button for closing the preview. The button's visibility is controlled by the `showDismissButton` parameter.
        -   The button is styled with an "xmark" icon on a red circle background, using the `onError` and `error` colors from the theme.
    -   **UI:**
        - The preview component is displayed within a `ZStack` with the image and dismiss button layered.
        - The component have max width and 144 in height.
        - The component is in a `RoundedRectangle` with corner radius 12.
    - **State Handling:**
        - The component uses `@State` to manage the `uiImage` and update the view when the image is loaded.
    - **Dynamic Updates:**
        - The preview updates when `imageUrl` changes.
    -   **Layout:**
        -   The component frame have a max width and a height of 144.
        -   The component clip to a `RoundedRectangle` with a corner radius of 12.
    -   **Preview:**
        -   Added a preview provider to quickly see how the component looks and how it react to different states.

---
+++ iosApp/iosApp/designSystem/component/image/TransactionImagePreView.swift
@@ -0,0 +1,62 @@
+import SwiftUI
+
+struct TransactionImagePreView: View {
+    
+    let imageUrl: String
+    var onDismiss: () -> Void = {}
+    var showDismissButton: Bool = true
+    @State private var uiImage: UIImage? = nil
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            ImageDisplay()
+            if showDismissButton {
+                DismissButton()
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: 144)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .onAppear {
+            loadImageFromAlbum()
+        }
+        .onChange(of: imageUrl) {
+            loadImageFromAlbum()
+        }
+    }
+    
+    @ViewBuilder
+    private func ImageDisplay() -> some View {
+        if let safeImage = uiImage {
+            Image(uiImage: safeImage).resizable()
+        } else {
+            Image(ImageConstants.fubukiStare).resizable()
+        }
+    }
+    
+    @ViewBuilder
+    private func DismissButton() -> some View {
+        Button(
+            action: onDismiss,
+            label: {
+                Image(systemName: "xmark")
+                    .tint(ColorTheme.onError)
+                    .imageScale(.small)
+                    .frame(width: 32, height: 32)
+                    .background(ColorTheme.error)
+                    .clipShape(Circle())
+            }
+        )
+        .padding(.vertical, 12)
+        .padding(.horizontal, 14)
+    }
+    
+    private func loadImageFromAlbum() {
+        Task {
+            uiImage = await fetchImage(localIdentifier: imageUrl)
+        }
+    }
+}
+
+#Preview {
+    TransactionImagePreView(imageUrl: "Hello world"){}
+}
