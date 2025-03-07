85a859d66fb3d31d93ef61efafd4765955a739b5
uwi0 <kakapoweb@gmail.com>
2025-03-07 14:12:58 +0700

    feat: refactor home screen UI and remove unused files
    This commit refactors the Home screen UI in the iOS application and removes unused Swift files related to the previous design.
    
    -   **Remove Unused Files:**
        - Removed `HomeContentView.swift` and `HomeTopBarView.swift` as they are no longer used in the refactored UI.
    -  **Update Home Screen**:
        - Move the previous `HomeContentView` code in `HomeScreen`
        - Remove `HomeTopBarView` and replace with new `TopAppBarView`.
        - Change `HomeScreen` view logic to use `ContentView` to render the screen.
        - Update the UI to reflect changes in state and event handling.
    - **General**:
        - Remove unused code in `HomeScreen`

--- iosApp/iosApp/screen/home/HomeScreen.swift
+++ iosApp/iosApp/screen/home/HomeScreen.swift
@@ -7,33 +7,24 @@ struct HomeScreen: View {
     @StateObject private var viewModel: HomeViewModel = HomeViewModel()
     @EnvironmentObject private var navigation: AppNavigation
     
+    private var uiState: HomeState {
+        viewModel.uiState
+    }
+    
     var body: some View {
         GeometryReader { geometryReader in
-            ZStack {
-                ColorTheme.surface
-                    .ignoresSafeArea(.all)
-                
-                VStack {
-                    HomeTopBarView(
-                        onClick: {
-                            viewModel.handle(event: .OpenDrawer())
-                        }
-                    )
-                    HomeContentView(
-                        uiState: viewModel.uiState,
-                        onEvent: viewModel.handle(event:)
-                    )
-                }
-                
-                FabButtonView(
-                    size: FabConstant.size,
-                    xPos: geometryReader.size.width - FabConstant.xOffset,
-                    yPos: geometryReader.size.height - FabConstant.yOffset,
-                    onClick: {
-                        navigation.navigate(to: .addTransaction(transactionId: 0))
-                    }
-                )
+            ColorTheme.surface
+                .ignoresSafeArea(.all)
+            VStack {
+                TopAppBarView()
+                ContentView()
             }
+            FabButtonView(
+                size: FabConstant.size,
+                xPos: geometryReader.size.width - FabConstant.xOffset,
+                yPos: geometryReader.size.height - FabConstant.yOffset,
+                onClick: { navigation.navigate(to: .addTransaction(transactionId: 0)) }
+            )
         }
         .navigationBarBackButtonHidden(true)
         .onAppear {
@@ -42,6 +33,47 @@ struct HomeScreen: View {
         .onChange(of: viewModel.uiEffects){
             observe(effect:viewModel.uiEffects)
         }
+    }
+    
+    @ViewBuilder
+    private func TopAppBarView() -> some View {
+        HStack(alignment: .center,spacing: 16) {
+            Image(systemName: "line.3.horizontal")
+                .tint(ColorTheme.onSurface)
+                .font(.title)
+                .onTapGesture { viewModel.handle(event: .OpenDrawer()) }
+            
+            Text("Dashboard")
+                .font(Typography.titleMedium)
+        }
+        .padding(.horizontal, 16)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    @ViewBuilder
+    private func ContentView() -> some View {
+        ScrollView {
+            VStack(alignment: .leading,spacing: 16) {
+                WalletBalanceView(wallet: uiState.wallet)
+                MonthlyBudgetView(monthlyBudgetOverView: uiState.monthlyBudgetOverView, onEvent: viewModel.handle(event:))
+                Text("RecentTransaction")
+                    .font(Typography.titleMedium)
+                HomeTransactionsView(transactions: uiState.transactions)
+                if uiState.transactions.count > 3 {
+                    ShowMoreItemView(onClick: { viewModel.handle(event: .ToTransactions())})
+                }
+                GoalHeaderView(isVisible: true, onClick: { viewModel.handle(event: .ToCreateGoal()) })
+                GoalsView(
+                    goals: uiState.goals,
+                    onClick: { goal in viewModel.handle(event: .ToGoalWith(id: goal.id))}
+                )
+                ShowMoreItemView(onClick: {})
+            }
+            .padding(.vertical, 24)
+            .padding(.horizontal, 16)
+            .ignoresSafeArea(.all)
+        }
+        .scrollIndicators(.hidden)
     }
     
     private func observe(effect: HomeEffect?) {
@@ -71,4 +103,8 @@ struct HomeScreen: View {
         }
         viewModel.uiEffects = nil
     }
+}
+
+#Preview {
+    HomeScreen(showDrawer: .constant(false))
 }
