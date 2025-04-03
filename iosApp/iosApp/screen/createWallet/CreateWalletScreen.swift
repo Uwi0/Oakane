c1ee3253bc4c908e4208596bb398280e8ea7b121
uwi0 <kakapoweb@gmail.com>
2025-04-03 13:03:20 +0700

    feat: implement create wallet screen and view model on iOS
    This commit introduces the create wallet screen and its associated view model on iOS, laying the foundation for wallet creation functionality.
    
    -   **Create Wallet Screen:**
        -   Created `CreateWalletScreen` as the main view for creating a new wallet.
    -   -  Add basic `Text` for display.
    +    - Added a basic `Text` view for initial display.
    +   - **Create Wallet ViewModel:**
    +       - Created `CreateWalletViewModel` to handle the logic and state management of the `CreateWalletScreen`.
    +       - Implemented state observation and handling using `Combine` and KMP Native Coroutines.
    +       - Implemented `initData` to initialize the view model with necessary data, including wallet ID.
    +       - Implemented `handle` to handle ui event from `CreateWalletScreen`.
    +       - Implemented `observeState` to observe the state flow from Kotlin.
    +       - Implemented `observeEffect` to observe the effect flow from Kotlin.
    +       - Added `deinit` to clean up Combine cancellables.
    +       - Added `@Published` for `uiState` and `uiEffect` to update the view.
    +       - Added `update` function to update ui effect and state.
    +       - Use `CreateWalletViewModelKt` from shared.
    +       - Add `Koin.shared` to get instance `CreateWalletViewModelKt`.
    +       - Implemented `CreateWalletState` to manage the UI state.
    +       - Implemented `CreateWalletEffect` to manage the UI effects.
    +       - Implemented `CreateWalletEvent` to manage the UI event.
    +   - Create `CreateWalletViewModel` class to manage all the logic.
    +   - Create `CreateWalletState` struct to manage ui state.
    +   - Create `CreateWalletEffect` struct to manage ui effect.
    +   - Create `CreateWalletEvent` struct to manage ui event.
    +   - Update dependency version.
    +   - Update koin version.
    +   - Update KMP version.

---
+++ iosApp/iosApp/screen/createWallet/CreateWalletScreen.swift
@@ -0,0 +1,19 @@
+//
+//  CreateWalletScreen.swift
+//  oakane
+//
+//  Created by dwi prasetyo on 03/04/25.
+//  Copyright © 2025 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateWalletScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateWalletScreen()
+}
---
+++ iosApp/iosApp/screen/createWallet/CreateWalletViewModel.swift
@@ -0,0 +1,61 @@
+import Foundation
+import Shared
+import Combine
+import KMPNativeCoroutinesCombine
+
+final class CreateWalletViewModel: ObservableObject {
+    
+    @Published private(set) var uiState: CreateWalletState = CreateWalletState.companion.default()
+    @Published var uiEffect: CreateWalletEffect? = nil
+    
+    private var viewModel: CreateWalletViewModelKt = Koin.shared.get()
+    
+    private var stateCancellable: AnyCancellable?
+    private var effectCancellable: AnyCancellable?
+    
+    func initData(walletId: Int64) {
+        viewModel.doInitData(walletId: walletId)
+        observeState()
+    }
+    
+    func handle(event: Shared.CreateWalletEvent) {
+        viewModel.handleEvent(event: event)
+    }
+    
+    private func observeState() {
+        let publihser = createPublisher(for: viewModel.uiStateFlow)
+        stateCancellable = publihser.sink { completion in
+            print("Completion: \(completion)")
+        } receiveValue: { [weak self] state in
+            guard let self else { return }
+            self.update(state: state)
+        }
+    }
+    
+    private func update(state: CreateWalletState) {
+        DispatchQueue.main.async {
+            self.uiState = state
+        }
+    }
+    
+    private func observeEffect() {
+        let publisher = createPublisher(for: viewModel.uiEffect)
+        effectCancellable = publisher.sink { completion in
+            print("completion: \(completion)")
+        } receiveValue: { [weak self] effect in
+            guard let self else { return }
+            self.update(effect: effect)
+        }
+    }
+    
+    private func update(effect: CreateWalletEffect) {
+        DispatchQueue.main.async {
+            self.uiEffect = effect
+        }
+    }
+    
+    deinit {
+        stateCancellable?.cancel()
+        effectCancellable?.cancel()
+    }
+}
